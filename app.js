51a5446a647962b5d736b2a98c3c0a1f03bdab94
barnescaleb <barnescaleb119@gmail.com>
2022-08-25 11:16:27 -0700

    Edit HTML and app.JS

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-let previousAction = null,
-    previousAnswer = null
 
-
-
-const allClear = () => {
-    let history = document.getElementById('history'),
-    input = document.getElementById('input')
-
-    history.textContent = ''
-    input.textContent = ''
-    previousAction = 'clear'
+// Basic operatation functions
+function add(a, b) {
+    return parseFloat(a) + parseFloat(b)
 }
-
-let buttons = Array.from(document.querySelectorAll('.buttons>button'))
-buttons.forEach(button => {
-    if (button.id == 'delete') {
-        button.addEventListener('click', () => {
-            let history = document.getElementById('history')
-            history.textContent = history.textContent.slice(0, -1);
-
-            if (previousAction == 'equals') {
-            
-            }
-        })
+function subtract(a, b) {
+    return parseFloat(a) - parseFloat(b)
+}
+function multiply(a, b) {
+    return parseFloat(a) * parseFloat(b)
+}
+function divide(a, b) {
+    return parseFloat(a) / parseFloat(b)
+};
+// Takes an operator & 2 numbers and calls one function above
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber,secondNumber);
+            break;
+        case '-':
+            return subtract(firstNumber,secondNumber);
+            break;
+        // case 'x':
+        //     return multiply(firstNumber,secondNumber);
+        //     break;
+        case '*':
+            return multiply(firstNumber,secondNumber);
+            break;
+        case '/':
+            return add(firstNumber,secondNumber);
+            break;
     }
-})
+};
+
+const calculator = document.getElementById('calculator');
+const display = document.getElementById('display');
+const userInput = document.getElementById('.user-input');
+const result = document.getElementById('.result');
+
+
+
+
